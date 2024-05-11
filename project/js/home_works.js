abe789e099c540d2567dbe1cb752d0a9bf558fba
Bekjan777 <47937052+Bekjan777@users.noreply.github.com>
2024-05-11 16:09:11 +0600

    Update home_works.js

--- project/js/home_works.js
+++ project/js/home_works.js
@@ -17,13 +17,58 @@ buttonGmail.onclick = () => {
 const parentBlock = document.querySelector(".parent_block")
 const childBlock = document.querySelector(".child_block")
 
-let cnt = 0;
+const maxWidthOfParent = parentBlock.offsetWidth - childBlock.offsetWidth
+const maxHeightOfParent = parentBlock.offsetHeight - childBlock.offsetHeight
+
+let cntX = 0;
+let cntY = 0;
 const movementOfChildBlock = () => {
-    cnt++
-    childBlock.style.left = `${cnt}px`
-    if(cnt<448){
+    if(cntX < maxWidthOfParent && cntY <= 0){
+        cntX++
+        childBlock.style.left = `${cntX}px`
+
+        requestAnimationFrame(movementOfChildBlock)
+    } else if(cntY <= maxHeightOfParent && cntX >= maxWidthOfParent){
+        cntY++
+        childBlock.style.top = `${cntY}px`
+        requestAnimationFrame(movementOfChildBlock)
+    } else if(cntY > maxHeightOfParent && cntX > 0){
+        cntX--
+        childBlock.style.left = `${cntX}px`
+        requestAnimationFrame(movementOfChildBlock)
+    } else if (cntX >= 0 && cntY > 0 ){
+        cntY--
+        childBlock.style.top = `${cntY}px`
         requestAnimationFrame(movementOfChildBlock)
     }
+
 }
 
 movementOfChildBlock()
+const cnt = document.querySelector("#seconds");
+const start = document.querySelector("#start");
+const stop = document.querySelector("#stop");
+const reset = document.querySelector("#reset");
+let interval;
+let counter = 0;
+
+start.onclick = () => {
+    if (!interval) {
+        interval = setInterval(() => {
+            counter += 1;
+            cnt.innerHTML = counter;
+        }, 1000);
+    }
+};
+
+stop.onclick = () => {
+    clearInterval(interval);
+    interval = null;
+};
+
+reset.onclick = () => {
+    counter = 0;
+    cnt.innerHTML = counter;
+    clearInterval(interval);
+    interval = null;
+};
